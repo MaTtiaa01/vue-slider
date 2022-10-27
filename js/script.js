f9397d3300a0aed7c67c45aa3687dc4dfd2a2651
MaTtiaa01 <mattia2001@live.it>
2022-10-27 16:02:05 +0200

    add infinite slideshow

--- js/script.js
+++ js/script.js
@@ -42,9 +42,29 @@ createApp({
                 'img/04.webp',
                 'img/05.webp',
             ],
+            texts : [
+                'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+                'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+                "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+                'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+                'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+            ]
         }
     },
     methods : {
-
+        nextSlide(){
+            if (this.imgActive === this.images.length -1) {
+                this.imgActive = 0
+            }else{
+                this.imgActive++
+            }
+        },
+        prevSlide(){
+            if (this.imgActive === 0) {
+                this.imgActive = this.images.length -1
+            }else{
+                this.imgActive--
+            }
+        }
     }
-})
+}).mount("#app")
